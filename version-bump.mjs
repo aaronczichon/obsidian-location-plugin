1def03a168efbc6cdc09fa5ee40fda090aad26a4
Aaron Czichon <mail@aaronczichon.de>
2024-04-23 10:35:19 -0300

    ci: fix version bump script
    refs: #9

--- version-bump.mjs
+++ version-bump.mjs
@@ -1,6 +1,6 @@
 import { readFileSync, writeFileSync } from "fs";
 
-const targetVersion = process.env.npm_package_version;
+const targetVersion = process.env.NPM_PACKAGE_VERSION;
 
 // read minAppVersion from manifest.json and bump version to target version
 let manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
@@ -9,9 +9,9 @@ manifest.version = targetVersion;
 writeFileSync("manifest.json", JSON.stringify(manifest, null, "\t"));
 
 // update versions.json with target version and minAppVersion from manifest.json
-let versions = JSON.parse(readFileSync("versions.json", "utf8"));
-versions[targetVersion] = minAppVersion;
-writeFileSync("versions.json", JSON.stringify(versions, null, "\t"));
+// let versions = JSON.parse(readFileSync("versions.json", "utf8"));
+// versions[targetVersion] = minAppVersion;
+// writeFileSync("versions.json", JSON.stringify(versions, null, "\t"));
 
 // update package.json with version update
 let packageJson = JSON.parse(readFileSync("package.json", "utf8"));
