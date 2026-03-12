502cb9c44c74c4595e756d37667ea08780b05c36
Aaron Czichon <github@aaronczichon.de>
2026-03-12 10:50:28 +0100

    chore: fixed version issues

--- version-bump.mjs
+++ version-bump.mjs
@@ -9,9 +9,9 @@ manifest.version = targetVersion;
 writeFileSync('manifest.json', JSON.stringify(manifest, null, '\t'));
 
 // update versions.json with target version and minAppVersion from manifest.json
-// let versions = JSON.parse(readFileSync("versions.json", "utf8"));
-// versions[targetVersion] = minAppVersion;
-// writeFileSync("versions.json", JSON.stringify(versions, null, "\t"));
+let versions = JSON.parse(readFileSync('versions.json', 'utf8'));
+versions[targetVersion] = minAppVersion;
+writeFileSync('versions.json', JSON.stringify(versions, null, '\t'));
 
 // update package.json with version update
 let packageJson = JSON.parse(readFileSync('package.json', 'utf8'));
